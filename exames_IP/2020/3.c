8324a68767aa110574d82de53d94119133605666
noslen-dev <79165069+noslen-dev@users.noreply.github.com>
2021-02-28 18:11:04 +0000

    Update 3.c

--- exames_IP/2020/3.c
+++ exames_IP/2020/3.c
@@ -4,14 +4,15 @@ void maiorPalavra(char frase[], char palavra[]){
 int i,len,index,pos,cnt,maior=0;
 for(i=0; frase[i]!='\0';++i);
 len=i;
-for(i=0; i<len;++i)
+for(i=0; i<len;++i){
   for(index=i,cnt=0; frase[i]!=' '&& frase[i]!='\0';++i,++cnt);
   if(cnt>maior){
     maior=cnt;
     pos=index;
   }
-for(i=0; frase[index]!=' ' && frase[index]!='\0';++i,++index)
-  palavra[i]=frase[index];
+}
+for(i=0; frase[pos]!=' ' && frase[pos]!='\0';++i,++pos)
+  palavra[i]=frase[pos];
 palavra[i]='\0';
 printf("%s",palavra);
 }
@@ -19,7 +20,7 @@ printf("%s",palavra);
 
 int main(){
 char palavra[100];
-maiorPalavra("ana mol crossing",palavra);
-puts(palavra);
+maiorPalavra("ana crossing idk man",palavra);
+
 return 0;
 }
